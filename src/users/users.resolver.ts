2c827e8543c149afa20d9df178262469e7cf4ed4
Mihai <mihai.cujba@indrivo.com>
2022-12-05 12:28:35 +0200

    Added hot reload

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,34 +1,19 @@
 import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { UsersService } from './users.service';
-import { CreateUserInput } from './dto/create-user.input';
-import { UpdateUserInput } from './dto/update-user.input';
+import { UserType } from './dto/create-user.dto';
+import { UserInput } from './dto/user.input';
 
-@Resolver('User')
+@Resolver()
 export class UsersResolver {
   constructor(private readonly usersService: UsersService) {}
 
-  @Mutation('createUser')
-  create(@Args('createUserInput') createUserInput: CreateUserInput) {
-    return this.usersService.create(createUserInput);
+  @Mutation(() => UserType)
+  async createUser(@Args('input') input: UserInput) {
+    return this.usersService.create(input);
   }
 
-  @Query('users')
-  findAll() {
+  @Query(() => [UserType])
+  async users() {
     return this.usersService.findAll();
-  }
-
-  @Query('user')
-  findOne(@Args('id') id: number) {
-    return this.usersService.findOne(id);
-  }
-
-  @Mutation('updateUser')
-  update(@Args('updateUserInput') updateUserInput: UpdateUserInput) {
-    return this.usersService.update(updateUserInput.id, updateUserInput);
-  }
-
-  @Mutation('removeUser')
-  remove(@Args('id') id: number) {
-    return this.usersService.remove(id);
   }
 }
